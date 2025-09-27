982dcfede3d22202d3de11899fddaab49442ac51
Abdelhak Rahmouni <rahmouni.abdelhak.1@gmail.com>
2025-09-27 16:14:41 +0200

    feat: implement sign-up page ui

---
+++ frontend/src/pages/sign-up/index.tsx
@@ -0,0 +1,67 @@
+import { Button, Container, TextField, Typography } from '@mui/material';
+import reactLogo from '@assets/react.svg';
+import { Link } from 'react-router';
+
+const SignUpPage = () => {
+  return (
+    <Container maxWidth="sm" style={{ marginTop: '2rem' }}>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <h1>StudySwap</h1>
+      <p>Ceci est la page d'inscription de l'application</p>
+      <TextField
+        label="Nom"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        size="small"
+      />
+      <TextField
+        label="Prénom"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        size="small"
+      />
+      <TextField
+        label="Email"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        size="small"
+      />
+      <TextField
+        label="Mot de passe"
+        type="password"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        size="small"
+      />
+      <TextField
+        label="Confimrmer mot de passe"
+        type="password"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        size="small"
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        fullWidth
+        style={{ marginTop: '1rem' }}
+      >
+        S'inscrire
+      </Button>
+      <Typography
+        variant="body2"
+        color="textSecondary"
+        style={{ marginTop: '1rem' }}
+      >
+        Vous avez déja un compte ?{" "}
+        <Link to="/login">Se connecter</Link>
+      </Typography>
+    </Container>
+  );
+};
+export default SignUpPage;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,14 @@
 import {  Routes, Route } from "react-router";
 import App from "../App";
 import LoginPage from "../pages/login";
+import SignUpPage from "@pages/sign-up";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route index element={<App />} />
       <Route path="login" element={<LoginPage />} />
+      <Route path="sign-up" element={<SignUpPage />} />
     </Routes>
   )
 }
